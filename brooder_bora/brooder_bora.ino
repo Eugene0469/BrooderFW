eafe890d9cd121354432a84c1a5c9ef60beaf5bf
Eugene Mwangi <mwangieugene@tingle.software>
2019-08-24 12:53:10 +0300

    Adds functionality for the LCD

--- brooder_bora/brooder_bora.ino
+++ brooder_bora/brooder_bora.ino
@@ -1,16 +1,34 @@
-#define ENABLE_DS18B20 0 //enables the DS18B20
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal.h>
+#include <DHT.h>
+
+#define ENABLE_DS18B20 1 //enables the DS18B20
 #define ENABLE_DHT22   1 //enables the dht22
+#define ENABLE_LCD     1 //enables the lcd
+
+/*Definitions for both the DHT22 Sensors */
+#define ENABLE_DHT1 1 //enables the first dht22 sensor
+#define ENABLE_DHT2 1 //enables the second dht22 sensor
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define DHTPIN_1 7     //pin for the first dht22 sensor
+#define DHTPIN_2 8     //pin for the second dht22 sensor
+
+/*Definitions for all the DS18B20 Sensors */ 
+#define ONE_WIRE_BUS 2  // Data wire is plugged into port 2 on the Arduino
+#define ENABLE_DREAD 1 //enables temperature data reading.
+#define ENABLE_AREAD 0 //enables device address reading 
+  
+#if ENABLE_LCD
+  // initialize the library by associating any needed LCD interface pin
+  // with the arduino pin number it is connected to
+  const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+  LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+#endif
 
 #if ENABLE_DHT22
-  #include <DHT.h>
-
-  #define ENABLE_DHT1 1 //enables the first dht22 sensor
-  #define ENABLE_DHT2 1 //enables the second dht22 sensor
-  #define DHTTYPE DHT22   // DHT 22  (AM2302)
-  
   #if ENABLE_DHT1
     //Constants
-    #define DHTPIN_1 7     //pin for the first dht22 sensor
     DHT dht_1(DHTPIN_1, DHTTYPE); //// Initialize first DHT sensor for normal 16mhz Arduino
 
     //Variables
@@ -20,7 +38,6 @@
   #endif
 
   #if ENABLE_DHT2
-   #define DHTPIN_2 8     //pin for the second dht22 sensor
    DHT dht_2(DHTPIN_2, DHTTYPE); //// Initialize second DHT sensor for normal 16mhz Arduino
     //Variables
     int chk_2;
@@ -30,35 +47,22 @@
 #endif   
 
 #if ENABLE_DS18B20
-  #include <OneWire.h>
-  #include <DallasTemperature.h>
-  
-  // Data wire is plugged into port 2 on the Arduino
-  #define ONE_WIRE_BUS 2
-  
-  //Select between reading of the address and reading of the temperature.
-  
-  #define ENABLE_DREAD 0 //enables temperature data reading.
-  #define ENABLE_AREAD 0 //enables device address reading 
-  
-  
   // Setup a oneWire instance to communicate with any OneWire devices
   OneWire oneWire(ONE_WIRE_BUS);
   
   // Pass our oneWire reference to Dallas Temperature.
   DallasTemperature sensors(&oneWire);
   #if ENABLE_AREAD
-  // variable to hold device addresses
-  DeviceAddress Thermometer;
-  
-  int deviceCount = 0;
+    // variable to hold device addresses
+    DeviceAddress Thermometer;
+    int deviceCount = 0;
   #endif
   
   #if ENABLE_DREAD
-  // Addresses of 3 DS18B20s.Replace with own addresses
-  uint8_t sensor1[8] = { 0x28, 0xEE, 0xD5, 0x64, 0x1A, 0x16, 0x02, 0xEC };
-  uint8_t sensor2[8] = { 0x28, 0x61, 0x64, 0x12, 0x3C, 0x7C, 0x2F, 0x27 };
-  uint8_t sensor3[8] = { 0x28, 0x61, 0x64, 0x12, 0x3F, 0xFD, 0x80, 0xC6 };
+    // Addresses of 3 DS18B20s.Replace with own addresses
+    uint8_t sensor1[8] = { 0x28, 0xEE, 0xD5, 0x64, 0x1A, 0x16, 0x02, 0xEC };
+    uint8_t sensor2[8] = { 0x28, 0x61, 0x64, 0x12, 0x3C, 0x7C, 0x2F, 0x27 };
+    uint8_t sensor3[8] = { 0x28, 0x61, 0x64, 0x12, 0x3F, 0xFD, 0x80, 0xC6 };
   #endif
 #endif
 
@@ -96,6 +100,10 @@ void setup(void)
     #if ENABLE_DHT2
       dht_2.begin();
     #endif
+   #endif
+   #if ENABLE_LCD
+     // set up the LCD's number of columns and rows:
+     lcd.begin(20, 4);
    #endif
 }
 
