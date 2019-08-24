d2a54b562b39c58ebeca9115c3f073b4435711c1
Eugene Mwangi <mwangieugene@tingle.software>
2019-08-24 11:13:20 +0300

    Reads the temperature fromeach DS18B20 sensor de

--- brooder_bora/brooder_bora.ino
+++ brooder_bora/brooder_bora.ino
@@ -10,49 +10,41 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
-// variable to hold device addresses
-DeviceAddress Thermometer;
-
-int deviceCount = 0;
+// Addresses of 3 DS18B20s
+uint8_t sensor1[8] = { 0x28, 0xEE, 0xD5, 0x64, 0x1A, 0x16, 0x02, 0xEC };
+uint8_t sensor2[8] = { 0x28, 0x61, 0x64, 0x12, 0x3C, 0x7C, 0x2F, 0x27 };
+uint8_t sensor3[8] = { 0x28, 0x61, 0x64, 0x12, 0x3F, 0xFD, 0x80, 0xC6 };
 
 void setup(void)
 {
-  // start serial port
   Serial.begin(9600);
-
-  // Start up the library
   sensors.begin();
-
-  // locate devices on the bus
-  Serial.println("Locating devices...");
-  Serial.print("Found ");
-  deviceCount = sensors.getDeviceCount();
-  Serial.print(deviceCount, DEC);
-  Serial.println(" devices.");
-  Serial.println("");
-  
-  Serial.println("Printing addresses...");
-  for (int i = 0;  i < deviceCount;  i++)
-  {
-    Serial.print("Sensor ");
-    Serial.print(i+1);
-    Serial.print(" : ");
-    sensors.getAddress(Thermometer, i);
-    printAddress(Thermometer);
-  }
 }
 
 void loop(void)
-{}
+{
+  sensors.requestTemperatures();
+  
+  Serial.print("Sensor 1: ");
+  printTemperature(sensor1);
+  
+  Serial.print("Sensor 2: ");
+  printTemperature(sensor2);
+  
+  Serial.print("Sensor 3: ");
+  printTemperature(sensor3);
+  
+  Serial.println();
+  delay(1000);
+}
 
-void printAddress(DeviceAddress deviceAddress)
-{ 
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    Serial.print("0x");
-    if (deviceAddress[i] < 0x10) Serial.print("0");
-    Serial.print(deviceAddress[i], HEX);
-    if (i < 7) Serial.print(", ");
-  }
-  Serial.println("");
+void printTemperature(DeviceAddress deviceAddress)
+{
+  float tempC = sensors.getTempC(deviceAddress);
+  Serial.print(tempC);
+  Serial.print((char)176);
+  Serial.print("C  |  ");
+  Serial.print(DallasTemperature::toFahrenheit(tempC));
+  Serial.print((char)176);
+  Serial.println("F");
 }
